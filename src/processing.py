f66a221552a6dcda7558aa6848ab7c5e273fb04d
Max <z.max83@mail.ru>
2025-03-06 19:56:59 +0300

    первый коммит для pool request

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(id_list: list, state: str = "EXECUTED") -> list:
+    """Функция фильтрации транзакций по параметру состояния"""
     filtered_list = []
     for id_number in id_list:
         if id_number.get("state", 0) == state:
@@ -16,3 +17,13 @@ print(
         ]
     )
 )
+
+
+def sort_by_date(id_list: list, reverse: bool = True):
+    """Функция сортировки по дате"""
+    sorted_list = sorted(id_list, key = lambda x:x["date"],reverse = reverse)
+    return sorted_list
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))

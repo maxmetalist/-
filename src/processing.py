3410fffa964e18b626589d338ee81ca82972dab2
Max <z.max83@mail.ru>
2025-03-06 20:22:00 +0300

    первый коммит для pool request

--- src/processing.py
+++ src/processing.py
@@ -19,11 +19,19 @@ print(
 )
 
 
-def sort_by_date(id_list: list, reverse: bool = True):
+def sort_by_date(id_list: list, reverse: bool = True) -> list:
     """Функция сортировки по дате"""
-    sorted_list = sorted(id_list, key = lambda x:x["date"],reverse = reverse)
+    sorted_list = sorted(id_list, key=lambda x: x["date"], reverse=reverse)
     return sorted_list
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)

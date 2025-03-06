3f9967bc9ff28fb66e3462616c29d6de4a9e70dc
Max <z.max83@mail.ru>
2025-03-06 19:40:01 +0300

    реализация функции фильтрации по параметру state

---
+++ src/processing.py
@@ -0,0 +1,18 @@
+def filter_by_state(id_list: list, state: str = "EXECUTED") -> list:
+    filtered_list = []
+    for id_number in id_list:
+        if id_number.get("state", 0) == state:
+            filtered_list.append(id_number)
+    return filtered_list
+
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)

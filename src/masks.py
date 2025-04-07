efa76e12c71e0f6a6080d316790ed9c1a34cdab5
Max <z.max83@mail.ru>
2025-04-07 22:31:03 +0300

    коммит к домашке 12_2

--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,24 @@
+import logging
+import os
+
+logger_masks = logging.getLogger("masks")
+file_handler = logging.FileHandler(
+    os.path.join(os.path.dirname(__file__), "..\\logs\\", "masks.log"), mode="w", encoding="utf-8"
+)
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s %(lineno)d")
+file_handler.setFormatter(file_formatter)
+logger_masks.addHandler(file_handler)
+logger_masks.setLevel(logging.DEBUG)
+
+
 def get_mask_card_number(card_number: str) -> str:
     """Функция маскирования номера карты"""
-
+    logger_masks.debug("Проверка формата номера карты для создания маски")
     if len(card_number) == 16 and card_number.isdigit():
+        logger_masks.debug("Вывод результата")
         return card_number[:4] + " " + card_number[4:6] + "** **** " + card_number[-4:]
     else:
+        logger_masks.error("Не верный формат номера карты")
         return "Номер карты должен содержать 16 цифр"
 
 
@@ -12,10 +27,12 @@ print(get_mask_card_number("1234567812345678"))
 
 def get_mask_account(account_number: str) -> str:
     """Функция маскирования номера счёта"""
-
+    logger_masks.debug("Проверка формата номера счёта для создания маски")
     if len(account_number) == 20 and account_number.isdigit():
+        logger_masks.debug("Вывод результата")
         return "**" + account_number[-4:]
     else:
+        logger_masks.error("Не верный формат номера счёта")
         return "Номер счёта должен содержать 20 цифр"
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,50 @@
 import json
+import logging
 import os
+
+logger_utils = logging.getLogger("utils")
+file_handler = logging.FileHandler(
+    os.path.join(os.path.dirname(__file__), "..\\logs\\", "utils.log"), mode="w", encoding="utf-8"
+)
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s %(lineno)d")
+file_handler.setFormatter(file_formatter)
+logger_utils.addHandler(file_handler)
+logger_utils.setLevel(logging.DEBUG)
 
 
 def load_operations(json_file_path):
     """Функция чтения объекта json из файла operations.json"""
     operations = []
     try:
+        logger_utils.debug("Запрос на чтение файла")
         with open(json_file_path, "r", encoding="utf-8") as json_file:
             if not json_file:
+                logger_utils.error("Файл не существует или пустой")
+                logger_utils.error("Результат: []")
                 return operations
             else:
+                logger_utils.debug("Декодирование файла в python-объект")
                 operations = json.load(json_file)
                 if type(operations) is not list:
+                    logger_utils.error("Файл находится не в формате списка")
+                    logger_utils.error("Результат: []")
                     return []
     except FileNotFoundError:
-        print(f"Файл {json_file_path} не найден")
+        logger_utils.error("Файл по указанному пути не найден")
+        # print(f"Файл {json_file_path} не найден")
     except json.JSONDecodeError:
-        print(f"Ошибка декодирования файла {json_file_path}")
-    except Exception:
-        print("Произошла ошибка")
+        logger_utils.error("Ошибка декодирования")
+        # print(f"Ошибка декодирования файла {json_file_path}")
+    except Exception as e:
+        logger_utils.error(f"Ошибка {e}")
+        # print(f"Произошла ошибка: {e}")
+    logger_utils.debug("Вывод результата")
     return operations
 
 
 def open_file():
     """Функция, задающая путь к файлу"""
-    json_file_path = os.path.join(os.path.dirname(os.getcwd()), "data", "operations.json")
+    json_file_path = os.path.join(os.path.dirname(__file__), "..\\data\\", "operations.json")
     data_ = load_operations(json_file_path)
     return data_
 
